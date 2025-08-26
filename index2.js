43a6a890462cabc045491533b51b144f0d4cf891
mehedihasanmehedi009 <mehedihasanmehedi007008@gmail.com>
2025-08-26 17:20:20 +0600

    mehedi hasan

---
+++ index2.js
@@ -0,0 +1,155 @@
+const validPin = 1234;
+const DateTime = [];
+//  function get value
+function getInputValueNumber(id) {
+  const values = parseInt(document.getElementById(id).value);
+  return values;
+}
+function getInputValue(id) {
+  const field = document.getElementById(id);
+  const fields = field.value;
+  return fields;
+}
+// function getInput innerText
+
+function getInputText(id) {
+  const eliment = document.getElementById(id);
+  const eliments = eliment.innerText;
+  const elimentes = parseInt(eliments);
+  return elimentes;
+}
+
+// function to set innerText
+function setInnerText(value) {
+  const valueNumber = document.getElementById("Balance");
+  valueNumber.innerText = value;
+}
+// add money function
+
+document.getElementById("btn-money").addEventListener("click", function (e) {
+  e.preventDefault();
+  const banks = document.getElementById("bank").value;
+
+  const Account = getInputValue("Account-Number");
+
+  const Amount = getInputValueNumber("Amount-to");
+
+  const pin = getInputValueNumber("add-pin");
+
+  const Balances = getInputText("Balance");
+
+  if (Account.length < 11) {
+    alert("please provide valid account number");
+    return;
+  }
+  if (pin !== validPin) {
+    alert("please provide valid pin number ");
+    return;
+  }
+
+  total = Amount + Balances;
+
+  setInnerText(total);
+
+  const data = {
+    Name: "Add Money",
+    date: new Date().toLocaleTimeString(),
+  };
+  DateTime.push(data);
+  console.log(DateTime);
+});
+
+// Withdraw-money
+document
+  .getElementById("Withdraw-money")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const Amounts = parseInt(document.getElementById("Withdraw-amount").value);
+    const balance = parseInt(document.getElementById("Balance").innerText);
+    const total = balance - Amounts;
+    setInnerText(total);
+
+    const data = {
+      Name: "Cash Out",
+      date: new Date().toLocaleTimeString(),
+    };
+    DateTime.push(data);
+    console.log(DateTime);
+  });
+
+// toggling -function
+function handleToggle(id) {
+  const forms = document.getElementsByClassName("form");
+
+  for (const form of forms) {
+    form.style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
+// border-color
+
+function background(id) {
+  const from = document.getElementsByClassName("form-btn");
+  for (const item of from) {
+    item.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+    item.classList.add("border-gray-200");
+  }
+  document.getElementById(id).classList.remove("border-gray-200");
+  document
+    .getElementById(id)
+    .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+}
+
+// exta
+
+document.getElementById("add-button").addEventListener("click", function (e) {
+  handleToggle("Add-Money-parent");
+  background("add-button");
+});
+document
+  .getElementById("Cash-Out-button")
+  .addEventListener("click", function () {
+    handleToggle("Cash-Out-Parent");
+    background("Cash-Out-button");
+  });
+document
+  .getElementById("Transfer-Money-button")
+  .addEventListener("click", function () {
+    const forms = document.getElementsByClassName("form");
+    for (const form of forms) {
+      form.style.display = "none";
+    }
+    document.getElementById("Transfer-Money").style.display = "block";
+    background("Transfer-Money-button");
+  });
+
+document
+  .getElementById("Get-Bonus-button")
+  .addEventListener("click", function () {
+    handleToggle("Get-Bonus");
+    background("Get-Bonus-button");
+  });
+
+document
+  .getElementById("Pay-Bill-button")
+  .addEventListener("click", function () {
+    handleToggle("Pay-Bill");
+    const from = document.getElementsByClassName("form-btn");
+    for (const item of from) {
+      item.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+      item.classList.add("border-gray-200");
+    }
+    document
+      .getElementById("Pay-Bill-button")
+      .classList.remove("border-gray-200");
+    document
+      .getElementById("Pay-Bill-button")
+      .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+  });
+
+document
+  .getElementById("TransacTions-button")
+  .addEventListener("click", function () {
+    handleToggle("Transaction-History");
+    background("TransacTions-button");
+  });
